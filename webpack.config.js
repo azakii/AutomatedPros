09bf04d93315f9fb053328b5f6b0b7c8a55dc3df
Ahmed zaki <ah.zakii@hotmail.com>
2024-01-05 15:40:07 +0300

    push

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'production', // or 'production'
+  mode: 'development', // or 'production'
   entry: './src/js/main.js',
 
   stats: {
